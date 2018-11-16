e43881cb0059b7eecf7f2c78bfcbadf2179179e6
Bogdan Posedaru <bogdan.posedaru@gmail.com>
2018-11-16 17:15:37 +0200

    Add GET totalPersons

--- server.js
+++ server.js
@@ -1,3 +1,5 @@
+const fs = require('fs');
+
 // server.js
 const jsonServer = require('json-server')
 const server = jsonServer.create()
@@ -7,7 +9,22 @@ const middlewares = jsonServer.defaults()
 const PORT = process.env.PORT || 3000;
 
 server.use(middlewares)
+
+server.get('/totalPersons', (req, res) => {
+  const db = fs.readFile('./db.json', 'utf-8', (err, data) => {
+    if (err) {
+      throw err;
+    }
+    const totalPersons = JSON.parse(data).persons.length;
+    res.send({
+      totalPersons
+    }).end()
+  })
+})
+
 server.use(router)
+
+
 server.listen(PORT, () => {
   console.log('JSON Server is running')
 })
